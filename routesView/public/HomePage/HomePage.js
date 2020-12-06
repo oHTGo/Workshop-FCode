a24c93c4435ffaa1b75af72393b7fca944cffb11
unknown <khoi@gmail.com>
2020-12-06 22:08:12 +0700

    add JS into Homepage

--- routesView/public/HomePage/HomePage.js
+++ routesView/public/HomePage/HomePage.js
@@ -31,8 +31,7 @@
 fetch("/api/topic")
   .then((response) => response.json())
   .then((data) => {
-    data.message.forEach((element) => {
-      console.log(element);
+    let postsArray = data.message.map((element) => {
       let weeks = [
         "Sunday",
         "Monday",
@@ -42,11 +41,35 @@ fetch("/api/topic")
         "Friday",
         "Saturday",
       ];
-      let postsArray = document.getElementsByClassName("weekDay");
       let postDate = new Date(element.date);
-      for (var i = 0; i < postsArray.length; i++) {
-        postsArray[i].innerHTML = `${weeks[postDate.getDay()]}`;
-      }
+
+      return `
+        <div class="blog-post">
+          <div class="blog-post__img">
+            <img src="../img/5.png" alt="">
+          </div>
+          <div class="blog-post__info">
+            <div class="blog-post__date">
+              <span class="weekDay">${weeks[postDate.getDay()]}</span>
+              <span class="formatDate">${postDate.toLocaleDateString()}</span>
+            </div>
+            <div class="blog-post__rate">
+              <span class="starRate">${element.review[0].star}</span>
+              <svg width="1.1em" height="1.1em" viewBox="0 0 16 16" class="bi bi-star-fill" fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+              </svg>
+            </div>
+            <h1 class="blog-post__title">${element.name}</h1>
+            <p class="blog-post__description">${element.detail}</p>
+            <a href="../SinglePost/SinglePost.html" class="blog-post__cta">Read more</a>
+          </div>
+        </div>
+      `;
     });
+
+    postsArray = postsArray.join("");
+    document.getElementById("homepagePosts").innerHTML = postsArray;
   })
   .catch((error) => console.log(error));
