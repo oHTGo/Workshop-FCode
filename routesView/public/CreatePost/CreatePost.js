046600e04f660bc120154321a37cff01c8352d63
unknown <khoi@gmail.com>
2020-12-06 23:09:09 +0700

    add JS into CreatePost

--- routesView/public/CreatePost/CreatePost.js
+++ routesView/public/CreatePost/CreatePost.js
@@ -1,3 +1,10 @@
 post = () => {
+    const postObj = {
+        name : document.getElementById('title-input').value,
+        date : document.getElementById('example-datetime-local-input').value,
+        detail : document.getElementById('main-content').value,
+        note : document.getElementById('post__note').value
+    }
+    console.log(postObj);
     
 }
