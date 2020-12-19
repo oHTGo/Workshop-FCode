051de18498c1e195c1a12dc8f7b15a7fd7edbace
oHTGo <nguyennhathuy.orit@gmail.com>
2020-12-19 17:15:06 +0700

    Update docs api

--- docs/api_data.js
+++ docs/api_data.js
@@ -204,6 +204,623 @@ define({ "api": [
     "name": "PutReviewTopicid"
   },
   {
+    "type": "delete",
+    "url": "/topic/:topicId",
+    "title": "5. Delete Topic",
+    "group": "Topic",
+    "error": {
+      "fields": {
+        "Response": [
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status when complete</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Message when complete</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routesAPI/topic.js",
+    "groupTitle": "Topic",
+    "name": "DeleteTopicTopicid"
+  },
+  {
+    "type": "get",
+    "url": "/topic",
+    "title": "1.  Get an accepted list topic",
+    "group": "Topic",
+    "success": {
+      "fields": {
+        "Success": [
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status of request</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object[]",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Array of object request</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object._id",
+            "description": "<p>ID of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.name",
+            "description": "<p>Name of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.detail",
+            "description": "<p>Detail of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.note",
+            "description": "<p>Note of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.background",
+            "description": "<p>URL background of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Date",
+            "optional": false,
+            "field": "object.date",
+            "description": "<p>Date start of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object[]",
+            "optional": false,
+            "field": "object.group",
+            "description": "<p>List members of group topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.group._id",
+            "description": "<p>ID of member's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.group.name",
+            "description": "<p>Name of member's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object",
+            "optional": false,
+            "field": "object.author",
+            "description": "<p>Information of author</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.author._id",
+            "description": "<p>ID of author's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.author.name",
+            "description": "<p>Name of author's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object[]",
+            "optional": false,
+            "field": "object.review",
+            "description": "<p>Object array have 1 element. It is a average rate of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object[]",
+            "optional": false,
+            "field": "object.participants",
+            "description": "<p>Object array have 1 element. It is a total participants of topic.</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error": [
+          {
+            "group": "Error",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status when complete</p>"
+          },
+          {
+            "group": "Error",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Message when complete</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routesAPI/topic.js",
+    "groupTitle": "Topic",
+    "name": "GetTopic"
+  },
+  {
+    "type": "get",
+    "url": "/topic/:topicId",
+    "title": "3. Get topic",
+    "group": "Topic",
+    "success": {
+      "fields": {
+        "Success": [
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status of request</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Array of object request</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object._id",
+            "description": "<p>ID of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.name",
+            "description": "<p>Name of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.detail",
+            "description": "<p>Detail of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.note",
+            "description": "<p>Note of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.background",
+            "description": "<p>URL background of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Date",
+            "optional": false,
+            "field": "object.date",
+            "description": "<p>Date start of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object[]",
+            "optional": false,
+            "field": "object.group",
+            "description": "<p>List members of group topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.group._id",
+            "description": "<p>ID of member's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.group.name",
+            "description": "<p>Name of member's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object[]",
+            "optional": false,
+            "field": "object.participants",
+            "description": "<p>List participants of topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.participants._id",
+            "description": "<p>ID of participants's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.participants.name",
+            "description": "<p>Name of participants's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object",
+            "optional": false,
+            "field": "object.author",
+            "description": "<p>Information of author</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.author._id",
+            "description": "<p>ID of author's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.author.name",
+            "description": "<p>Name of author's topic</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object[]",
+            "optional": false,
+            "field": "object.review",
+            "description": "<p>Review of members</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "object.review.reviewOfUser",
+            "description": "<p>Content of review</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Number",
+            "optional": false,
+            "field": "object.review.star",
+            "description": "<p>Star of review</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error": [
+          {
+            "group": "Error",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status when complete</p>"
+          },
+          {
+            "group": "Error",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Message when complete</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routesAPI/topic.js",
+    "groupTitle": "Topic",
+    "name": "GetTopicTopicid"
+  },
+  {
+    "type": "get",
+    "url": "/topic/:topicId/join",
+    "title": "6.  Get status of participant's topic",
+    "group": "Topic",
+    "success": {
+      "fields": {
+        "Success": [
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status of request</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Object",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Object request</p>"
+          },
+          {
+            "group": "Success",
+            "type": "Boolean",
+            "optional": false,
+            "field": "object.statusParticipant",
+            "description": "<p>Status of participant's topic</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error": [
+          {
+            "group": "Error",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status when complete</p>"
+          },
+          {
+            "group": "Error",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Message when complete</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routesAPI/topic.js",
+    "groupTitle": "Topic",
+    "name": "GetTopicTopicidJoin"
+  },
+  {
+    "type": "post",
+    "url": "/topic",
+    "title": "2. Create Topic",
+    "group": "Topic",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "name",
+            "description": "<p>Name of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "detail",
+            "description": "<p>Detail of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "note",
+            "description": "<p>Note of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "background",
+            "description": "<p>URL background of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Date",
+            "optional": false,
+            "field": "date",
+            "description": "<p>Date start of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Array",
+            "optional": false,
+            "field": "group",
+            "description": "<p>Array userID is members</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Response": [
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status when complete</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Message when complete</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routesAPI/topic.js",
+    "groupTitle": "Topic",
+    "name": "PostTopic"
+  },
+  {
+    "type": "post",
+    "url": "/topic/:topicId/join",
+    "title": "7.  Join topic",
+    "group": "Topic",
+    "success": {
+      "fields": {
+        "Success": [
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status of request</p>"
+          },
+          {
+            "group": "Success",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Message of request</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error": [
+          {
+            "group": "Error",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status when complete</p>"
+          },
+          {
+            "group": "Error",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Message when complete</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routesAPI/topic.js",
+    "groupTitle": "Topic",
+    "name": "PostTopicTopicidJoin"
+  },
+  {
+    "type": "put",
+    "url": "/topic/:topicId",
+    "title": "4. Update Topic",
+    "group": "Topic",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "name",
+            "description": "<p>Name of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "detail",
+            "description": "<p>Detail of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "note",
+            "description": "<p>Note of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "background",
+            "description": "<p>URL background of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Date",
+            "optional": false,
+            "field": "date",
+            "description": "<p>Date start of topic</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Array",
+            "optional": false,
+            "field": "group",
+            "description": "<p>Array userID is members</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Response": [
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Status when complete</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Message when complete</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routesAPI/topic.js",
+    "groupTitle": "Topic",
+    "name": "PutTopicTopicid"
+  },
+  {
     "type": "get",
     "url": "/user",
     "title": "1. Get lists of user information",
--- docs/api_project.js
+++ docs/api_project.js
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-19T06:59:18.529Z",
+    "time": "2020-12-19T10:13:53.656Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
