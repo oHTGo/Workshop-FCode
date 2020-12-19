2e31ffd84721630a7333d270ff3a92c8d2a5ebba
oHTGo <nguyennhathuy.orit@gmail.com>
2020-12-19 17:38:57 +0700

    Fix docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-19T10:13:53.656Z",
+    "time": "2020-12-19T10:38:47.973Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
