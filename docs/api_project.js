1498054f153eac743da2164c40e29bf80b820707
oHTGo <69106844+oHTGo@users.noreply.github.com>
2020-12-19 17:10:00 +0700

    Revert "Update docs api"

--- docs/api_project.js
+++ docs/api_project.js
@@ -2,19 +2,18 @@ define({
   "name": "Backend for workshop",
   "description": "",
   "title": "API for Workshop",
-  "url": "http://localhost:8080/api",
+  "url": "https://localhost:8080/api",
   "template": {
-    "forceLanguage": "en",
-    "withCompare": false,
-    "withGenerator": false
+    "forceLanguage": "en"
   },
+  "withCompare": false,
   "version": "1.0.0",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-19T06:59:18.529Z",
+    "time": "2020-12-18T12:26:56.644Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
