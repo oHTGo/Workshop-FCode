2e3f1ae7d9aa8a212c68f405362716c20a497b88
unknown <khoi@gmail.com>
2020-12-20 22:28:11 +0700

    update comment section

--- routesView/public/SinglePost/SinglePost.js
+++ routesView/public/SinglePost/SinglePost.js
@@ -1,32 +1,79 @@
+
+
 function getCurrentUser() {
   fetch("/api/user/current")
     .then((res) => res.json())
     .then((data) => {
       myStorage.setItem("CurrentUserId", data.message._id);
       myStorage.setItem("CurrentUsername", data.message.name);
-      document.getElementById('userAccount').insertAdjacentHTML("beforeend", data.message.name);
+      document
+        .getElementById("userAccount")
+        .insertAdjacentHTML("beforeend", data.message.name);
+    })
+    .catch((error) => console.log(error));
+}
+getCurrentUser();
+console.log("Current topic id: " + window.localStorage.topicId);
+
+/* ------------- Manage Join status of users ------------*/
+let current_state, states = ["JOIN US", "JOINED"],
+currentTopic = window.localStorage.topicId;
+ 
+function getJoinStatus() {
+  return fetch("/api/topic/" + window.localStorage.topicId + "/join")
+    .then((res) => res.json())
+    .then((data) => {
+      let statusParticipant = data.message.statusParticipant;
+      console.log(statusParticipant);
+      return statusParticipant;
     })
     .catch((error) => console.log(error));
 }
 
-getCurrentUser();
+function joinTopic(topicId) {  // this function is to post the join status to server
+  participantObj = {
+    statusParticipant: current_state,
+  };
+  console.log(participantObj);
 
+  fetch("/api/topic/" + topicId + "/join", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(participantObj),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
-var states = ["JOIN US", "JOINED"],
-  current_state = getJoinStatus(),
-  currentTopic = window.localStorage.topicId;
+async function renderInitialJoinButton() {
+  current_state = await getJoinStatus();
+  document.getElementById("joinedButton").innerHTML = `${
+    states[current_state ? 1 : 0]
+  }`;
+  if (current_state) {
+    document.getElementById("joinedButton").classList.toggle("disabledButton");
+  }
+}
 
-console.log("Current topic id: " + window.localStorage.topicId);
-function changeState() {
-  current_state = !current_state;
+async function changeState() {
+  (current_state = await getJoinStatus()), (current_state = !current_state);
   document.getElementById("joinedButton").innerHTML = `${
     states[current_state ? 1 : 0]
   }`;
   document.getElementById("joinedButton").classList.toggle("disabledButton");
-  joinTopic();
+  joinTopic(window.localStorage.topicId);
   return current_state;
 }
+/*-----------------------------------------------------------------------------------*/
 
+/*---------------------- Manage rating action of users ---------------*/
 var count;
 function starmark(item) {
   count = item.id[0];
@@ -124,41 +171,16 @@ function review() {
     .then((data) => {})
     .catch((error) => console.log(error));
 }
+/*--------------------------------------------------------------------*/
 
-function getJoinStatus() {
-  fetch("/api/topic/" + currentTopic + "/join")
-    .then((res) => res.json())
-    .then((data) => {
-      let statusParticipant = data.message.statusParticipant;
-      console.log(statusParticipant);
-      return statusParticipant;
-
-    })
-    .catch((error) => console.log(error));
+/*--------------------- Admin action -------------------*/
+function rejectPost() {
+  fetch('')
 }
 
-function joinTopic() {
-  participantObj = {
-    statusParticipant: current_state,
-  };
-  console.log(participantObj);
+/*------------------------------------------------------*/
 
-  fetch("/api/topic/" + currentTopic + "/join", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(participantObj),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-}
-
+/*-------------------- Manage posts action ---------------------*/
 function deletePost(id) {
   fetch("/api/topic/" + id, {
     method: "DELETE",
@@ -166,9 +188,8 @@ function deletePost(id) {
     .then((res) => res.json())
     .then((res) => {
       console.log(res);
-      window.location.replace("../HomePage/HomePage.html");
+      // window.location.replace("../HomePage/HomePage.html");
     });
-  //window.location.replace("../HomePage/HomePage.html");
 }
 
 myStorage = window.localStorage;
@@ -216,49 +237,84 @@ function getSinglePost() {
           } else {
             partner = "";
           }
-          let postHeader = `<div class="post__header" id="post-header">
-          <div class="blog-post__icon">
-            <a href="../CreatePost/CreatePost.html" ><img src="../img/edit.svg" class="blog-post__edit"></a>
-            <a ><img src="../img/delete.svg" onClick="deletePost('${myStorage.topicId}')"></a>
-            
-          </div>
-          <div class="header__title">${element.name}</div>
+          let postHeader;
 
-          <div class="header__author">
-            <span>Author: </span>
-            <span>${element.author.name} ${partner}</span>
-          </div>
-          <div class="header__date">
-            <span>Date: </span>
-            <span>${postDateFormat}</span>
-          </div>
-          <div class="blog-post__rate" id="blog-post__rate">
-            <span>Rate: </span>
-            <span>${element.review[0]}</span>
-            <svg
-              width="1.1em"
-              height="1.1em"
-              viewBox="0 0 16 16"
-              class="bi bi-star-fill"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
-              />
-            </svg>
-          </div> 
-        </div>`;
+          // Differ between members and admin
+          if (myStorage.CurrentUserId === "5fc7646967708345bc3c5876") {
+            postHeader = `<div class="post__header" id="post-header">
+            <div class="blog-post__icon">
+              <a ><img src="../img/rejected.svg"></a>
+              <a href="#" ><img src="../img/accept.svg" class="blog-post__edit"></a>
+            </div>
+            <div class="header__title">${element.name}</div>
+              <div class="header__author">
+              <span>Author: </span>
+              <span>${element.author.name} ${partner}</span>
+            </div>
+            <div class="header__date">
+              <span>Date: </span>
+              <span>${postDateFormat}</span>
+            </div>
+            <div class="blog-post__rate" id="blog-post__rate">
+              <span>Rate: </span>
+              <span>${element.review[0]}</span>
+              <svg
+                width="1.1em"
+                height="1.1em"
+                viewBox="0 0 16 16"
+                class="bi bi-star-fill"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
+                />
+              </svg>
+            </div> 
+          </div>`;
+          } else {
+            postHeader = `<div class="post__header" id="post-header">
+            <div class="blog-post__icon">
+              <a ><img src="../img/delete.svg" onClick="deletePost('${myStorage.topicId}')"></a>  
+              <a href="../CreatePost/CreatePost.html" ><img src="../img/edit.svg" class="blog-post__edit"></a>
+            </div>
+            <div class="header__title">${element.name}</div>
+              <div class="header__author">
+              <span>Author: </span>
+              <span>${element.author.name} ${partner}</span>
+            </div>
+            <div class="header__date">
+              <span>Date: </span>
+              <span>${postDateFormat}</span>
+            </div>
+            <div class="blog-post__rate" id="blog-post__rate">
+              <span>Rate: </span>
+              <span>${element.review[0]}</span>
+              <svg
+                width="1.1em"
+                height="1.1em"
+                viewBox="0 0 16 16"
+                class="bi bi-star-fill"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
+                />
+              </svg>
+            </div> 
+          </div>`;
+          }
 
           let postBody = `<div class="post__body">
-          <img src="../img/5.png" />
-          <p>
-            ${element.detail}
-          </p>
-          <div class="alert alert-success" role="alert">
-            ${element.note}
-          </div>
-        </div>`;
+            <img src="${element.background}" />
+            <p>
+              ${element.detail}
+            </p>
+            <div class="alert alert-success" role="alert">
+              ${element.note}
+            </div>
+          </div>`;
 
           document
             .getElementById("singlepost-wrapper")
@@ -267,7 +323,7 @@ function getSinglePost() {
             .getElementById("post-header")
             .insertAdjacentHTML("afterend", postBody);
 
-          if (element.review[0] == null) {
+          if (element.review[0] == null || element.review[0] == 0) {
             document.getElementById("blog-post__rate").style.display = "none";
           }
         }
@@ -276,6 +332,66 @@ function getSinglePost() {
     .catch((error) => console.log(error));
 }
 
+var renderCommentsArray = [];
+function loadComments() {
+  return fetch("/api/topic/" + currentTopic)
+    .then((response) => response.json())
+    .then((data) => {
+      renderCommentsArray = data.message.review.map((element) => {
+        return `
+      <div class="comment__box">
+      <div class="comment__text">${element.reviewOfUser}</div>
+      <div class="blog-post__rate comment__rate">
+        <span>${element.star}</span>
+        <svg
+          width="1.3em"
+          height="1.3em"
+          viewBox="0 0 16 16"
+          class="bi bi-star-fill"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
+          />
+        </svg>
+      </div> 
+    </div>`;
+      });
+      // document.getElementById("comment-wrapper").innerHTML = renderArray;
+    })
+    .catch((error) => console.log(error));
+}
+
+let currentIndex = 0;
+async function renderComments() {
+  await loadComments();
+  const maxEachTurn = 1;
+  for (var i = 0; i < maxEachTurn; i++) {
+    if (currentIndex >= renderCommentsArray.length) {
+      $("#loadmore-button").hide();
+      return;
+    }
+    $("#comment-wrapper").append(renderCommentsArray[i + currentIndex]);
+  }
+  currentIndex += maxEachTurn;
+}
+
+function renderLoadmoreButton() {
+  let loadmoreButton = `<div class="loadmore-button__wrapper">
+      <div class="loadmore-button" id="loadmore-button" onClick="renderComments()">Load more <i class="fas fa-arrow-down"></i></div>
+    </div>`;
+  document
+    .getElementById("comment-wrapper")
+    .insertAdjacentHTML("afterend", loadmoreButton);
+}
+/*----------------------------------------------------------*/
+
+
+
+
 getSinglePost();
 getReview();
-
+renderLoadmoreButton();
+renderComments();
+renderInitialJoinButton();
