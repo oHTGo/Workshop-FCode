db7f1fa1dd972583130e3bfa057c538608919a38
unknown <khoi@gmail.com>
2020-12-23 20:19:31 +0700

    commit UPDATE function

--- routesView/public/CreatePost/CreatePost.js
+++ routesView/public/CreatePost/CreatePost.js
@@ -1,83 +1,132 @@
-fetch("/api/user")
-  .then((response) => response.json())
-  .then((data) => {
-    let usersArray = data.message.map((user) => {
-      return `<option value="${user._id}">${user.name}</option>`;
-    });
-    currentUser = `<option value="${myStorage.CurrentUserId}">${myStorage.CurrentUsername}</option>`
-    
-    usersArray.splice(usersArray.indexOf(currentUser), 1); // delete current account from partner list
+console.log(window.localStorage);
+function loadUserList() {
+  return fetch("/api/user")
+    .then((response) => response.json())
+    .then((data) => {
+      let usersArray = data.message.map((user) => {
+        return `<option id="${user._id}" value="${user._id}">${user.name}</option>`;
+      });
+      currentUser = `<option id="${window.localStorage.CurrentUserId}" value="${window.localStorage.CurrentUserId}">${window.localStorage.CurrentUsername}</option>`;
 
-    document
-      .getElementById("defaultOption")
-      .insertAdjacentHTML("afterend", usersArray);
-  })
-  .catch((error) => console.log(error));
+      usersArray.splice(usersArray.indexOf(currentUser), 1); // delete current account from partner list
 
-  
-post = () => {
+      document
+        .getElementById("defaultOption")
+        .insertAdjacentHTML("afterend", usersArray);
+    })
+    .catch((error) => console.log(error));
+}
+
+async function post() {
+  await loadUserList();
   let partner = document.getElementById("partnerSelection");
-  
-
   let groupAuthor = [
     {
       _id: document.getElementById("partnerSelection").value,
       // name: partner.options[partner.selectedIndex].text,
-    }
+    },
   ];
 
   let postObj = {};
-  if (groupAuthor[0]._id === '0') {
+  if (groupAuthor[0]._id === "0") {
     postObj = {
       name: document.getElementById("title-input").value,
       date: document.getElementById("example-datetime-local-input").value,
-      detail: CKEDITOR.instances['main-content'].getData(),
+      detail: CKEDITOR.instances["main-content"].getData(),
       note: document.getElementById("post__note").value,
-      background: document.getElementById('imageUpload').value      
-    }
+      background: document.getElementById("imageUpload").value,
+    };
   } else {
     postObj = {
       name: document.getElementById("title-input").value,
       date: document.getElementById("example-datetime-local-input").value,
-      detail: CKEDITOR.instances['main-content'].getData(),
+      detail: CKEDITOR.instances["main-content"].getData(),
       note: document.getElementById("post__note").value,
-      background: document.getElementById('imageUpload').value,   
+      background: document.getElementById("imageUpload").value,
       group: groupAuthor,
     };
-  };
+  }
   console.log(postObj);
-  fetch('/api/topic', {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(postObj),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data);
-      // alert('Succsessfully post!');
-      // window.location.reload();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-};
 
-let myStorage = window.localStorage
+  if (window.localStorage.topicId === "") {
+    fetch("/api/topic", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postObj),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        // alert('Succsessfully post!');
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  } else {
+    fetch("/api/topic/" + window.localStorage.topicId, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postObj),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        alert('Succsessfully update!');
+        //window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+}
+
+function renderEditPost(id) {
+  if (id !== "") {
+    fetch("/api/topic/" + id)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        let time = new Date(data.message.date).toTimeString().slice(0, 8);
+        let date = new Date(data.message.date).toISOString().slice(0, 11);
+        let datetime = date.concat(time);
+
+        if (data.message.group.length !== 0) {
+          // check if there is partner or not
+          document
+            .getElementById(data.message.group[0]._id)
+            .setAttribute("selected", "true");
+        }
+        document.getElementById(
+          "example-datetime-local-input"
+        ).value = datetime;
+        document.getElementById("title-input").value = data.message.name;
+        document.getElementById("imageUpload").value = data.message.background;
+        document.getElementById("main-content").innerHTML = data.message.detail;
+        document.getElementById("post__note").value = data.message.note;
+      })
+      .catch((error) => console.log(error));
+  }
+}
+
+let myStorage = window.localStorage;
 function getCurrentUser() {
   fetch("/api/user/current")
     .then((res) => res.json())
     .then((data) => {
       myStorage.setItem("CurrentUserId", data.message._id);
       myStorage.setItem("CurrentUsername", data.message.name);
-      document.getElementById('userAccount').insertAdjacentHTML("beforeend", data.message.name);
+      document
+        .getElementById("userAccount")
+        .insertAdjacentHTML("beforeend", data.message.name);
     })
     .catch((error) => console.log(error));
 }
 
+loadUserList();
 getCurrentUser();
-
-
-
-
+renderEditPost(window.localStorage.topicId);
